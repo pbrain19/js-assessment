da060598050527b451ffa9dd4cd3aec4e3392f37
pbrain19 <pbrain19@gmail.com>
2016-01-25 17:35:34 -0500

    got the async tests done.

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var q = $.Deferred();
+    setTimeout(function() {
+      q.resolve(value);
+    }, 0);
+    return q.promise()
   },
 
   manipulateRemoteData : function(url) {
-
+    var q = $.Deferred();
+    $.get(url).success(function(response) {
+      var people = response.people;
+      var names = _.pluck(people, 'name');
+      names = _.sortBy(names, function(name) {
+        return name;
+      });
+      q.resolve(names);
+    })
+    return q.promise()
   }
 };
